37818965b11f03ffa6a218097307635a41bc3dcd
Umang Bhutoria <umangbhutoria@Umangs-MacBook-Air.local>
2021-02-24 20:53:12 +0530

    First addition

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<str:page>",views.page, name="page")
 ]
