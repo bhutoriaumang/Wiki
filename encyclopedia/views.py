6b3226e9d199f53b3dd6082a2d4168676efdb825
Umang Bhutoria <umangbhutoria@Umangs-MacBook-Air.local>
2021-02-27 17:43:15 +0530

    Linked form input with entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import util
 from markdown2 import Markdown
 
@@ -9,10 +9,13 @@ def index(request):
     })
 
 def page(request):
-    markdowner = Markdown()
-    return render(request, "encyclopedia/page.html",{
-        "page": markdowner.convert(util.get_entry(request.GET.get("page")))
-    })
+    path = "/encyclopedia/"+request.GET.get("page")
+    return redirect(path)
+    #return entry(request,request.GET.get("page"))
+    #markdowner = Markdown()
+    #return render(request, "encyclopedia/page.html",{
+    #    "page": markdowner.convert(util.get_entry(request.GET.get("page")))
+    #})
 
 def entry(request,entry):
     markdowner = Markdown()
