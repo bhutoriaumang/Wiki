0685b608973f9e845f8c53790b4f6ecda144fd4c
Hrithik Agarwal <agarwal.hrithik3961@gmail.com>
2021-02-28 14:53:26 +0530

    Error page done

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,5 @@ app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("page",views.page, name="page"),
-    path("<str:entry>",views.entry, name="entry")
+    path("<str:entry>/",views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render,redirect
 from . import util
 from markdown2 import Markdown
 
-
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
@@ -11,14 +10,12 @@ def index(request):
 def page(request):
     path = "/encyclopedia/"+request.GET.get("page")
     return redirect(path)
-    #return entry(request,request.GET.get("page"))
-    #markdowner = Markdown()
-    #return render(request, "encyclopedia/page.html",{
-    #    "page": markdowner.convert(util.get_entry(request.GET.get("page")))
-    #})
 
 def entry(request,entry):
     markdowner = Markdown()
+    if util.get_entry(entry) == None:
+        return render(request, "encyclopedia/error.html")
     return render(request, "encyclopedia/page.html",{
-        "page": markdowner.convert(util.get_entry(entry))
+        "page": markdowner.convert(util.get_entry(entry)),
+        "entry": entry
     })
